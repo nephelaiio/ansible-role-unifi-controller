3c4496dcc4e22ac1fea8ba9b06fdc5056b4ac645
Ted Cook <teodoro.cook@gmail.com>
2017-04-15 18:27:23 -0600

    Fix service tests

--- tests/test_services.py
+++ tests/test_services.py
@@ -2,9 +2,9 @@ from testinfra.utils.ansible_runner import AnsibleRunner
 
 
 testinfra_hosts = \
-        AnsibleRunner('.molecule/ansible_inventory').get_hosts('test')
+    AnsibleRunner('.molecule/ansible_inventory').get_hosts('test')
 
 
 def test_command(Socket):
-    assert Socket('tcp://:::8080').is_listening
-    assert Socket('tcp://:::8843').is_listening
+    assert Socket('tcp://0.0.0.0:8080').is_listening
+    assert Socket('tcp://0.0.0.0:8843').is_listening
