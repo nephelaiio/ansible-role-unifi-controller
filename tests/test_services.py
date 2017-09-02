5055d287a6cf57cb0231ad176001d1e5477e03e6
Ted Cook <teodoro.cook@gmail.com>
2017-09-01 22:10:04 -0600

    Add support for molecule 2.0

--- tests/test_services.py
+++
@@ -1,9 +0,0 @@
-from testinfra.utils.ansible_runner import AnsibleRunner
-
-
-testinfra_hosts = \
-    AnsibleRunner('.molecule/ansible_inventory').get_hosts('test')
-
-
-def test_service(Service):
-    assert Service('unifi').is_running
