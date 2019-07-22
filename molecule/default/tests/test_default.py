fbff0182e78538d4211fc9bdc32ba16313b38d0c
Ted Cook <teodoro.cook@gmail.com>
2019-07-21 23:07:44 -0600

    Fix testinfra deprecation errors

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,6 +6,6 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_service(Service):
-    assert Service('unifi').is_enabled
-    assert Service('unifi').is_running
+def test_service(host):
+    assert host.service('unifi').is_enabled
+    assert host.service('unifi').is_running
